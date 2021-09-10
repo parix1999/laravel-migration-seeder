1f04684a5917a6609b431692433a3a8c8b8f0302
parix1999 <parisottoedoardo@gmail.com>
2021-09-10 17:18:07 +0200

    creazione del model per il passaggio dati

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Trip;
+
 
 class Home extends Controller
 
 {
     // Funzione per passare il tutto al file web per il return:
     public function index() {
-        return view('HomePage');
+        // return view('HomePage');
+
+        $allTrip = Trip::all();
+        
+        // Devo passare i dati alla view, che si occupa di mostrarli cosi:
+        // Specifico prima il file View e poi dentro compact ci metto i dati da passare:
+        return view('HomePage', compact('allTrip'));
+
     }
 }
---
+++ app/Trip.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model
+
+{
+    // Qui dentro ci sono tutti i dati del database basta passarli al controller.
+    
+}
