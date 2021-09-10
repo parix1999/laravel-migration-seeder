119120846c6eda510b865b6eded50cec26d20318
parix1999 <parisottoedoardo@gmail.com>
2021-09-10 15:15:14 +0200

    creazione del database e tabella

---
+++ database/migrations/2021_09_10_130353_create_trips_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('city', 50);
+            $table->double('price', 8, 2);
+            $table->text('cover');
+            $table->text('abstract');
+            $table->string('famousPlace', 255);
+            $table->string('trattamento', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
